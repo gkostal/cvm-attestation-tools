9fc9fa2973dbdf7b95d8f234b847a07c42153aac
Greg Kostal <gkostal@microsoft.com>
2025-08-03 07:24:52 -0700

    WIP

---
+++ web-api-service/create.dev.container.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+rem Create a long running development container using Docker
+
+rem Check if container already exists
+docker container inspect web-api-dev >nul 2>&1
+
+IF %ERRORLEVEL% EQU 0 (
+    echo Container 'web-api-dev' already exists.
+    echo Starting the container...
+    docker start -ai web-api-dev
+) ELSE (
+    echo Creating and starting 'web-api-dev' container...
+    docker run -dit ^
+        --name web-api-dev ^
+        --restart unless-stopped ^
+        ubuntu:24.04 ^
+        bash
+)
